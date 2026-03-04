dcdbe05b379f83e4830cfaa206901ca26d84295c
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-04 13:46:25 +0100

    setting up event struct

---
+++ internal/event/event.go
@@ -0,0 +1,37 @@
+package event
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Event struct {
+	ID string						`json:"event_id" bson:"event_id"`
+	Type string     				`json:"type" bson:"event_type"`
+	Data map[string]interface{} 	`json:"data" bson:"data"`
+	Timestamp time.Time              `json:"timestamp" bson:"timestamp"`
+	Processed bool                     `json:"processed" bson:"processed"`
+	ObjectID primitive.ObjectID       `json:"-" bson:"_id,omitempty"`
+}
+
+type CreateEventRequest struct {
+	Type string      				`json:"type"`
+	Data map[string]interface{} 	`json:"data"`
+}
+
+type CreateEventResponse struct {
+	EventId string `json:"event_id"`
+	Status string `json:"status"`
+}
+
+
+func NewEvent(eventType string, data map[string]interface{}) *Event {
+	return &Event{
+		ID:        "evt_" + primitive.NewObjectID().Hex()[:12],
+		Type:      eventType,
+		Data:      data,
+		Timestamp: time.Now().UTC(),
+		Processed: false,
+	}
+}
