d2b0b738167f2c428ab6cbda59bbbc6ae81cba20
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-04 13:49:24 +0100

    added validator

---
+++ internal/event/validator.go
@@ -0,0 +1,22 @@
+package event
+
+import "errors"
+
+var(
+	ErrInvalidEventType = errors.New("event type is required")
+	ErrInvalidEventData = errors.New("event data is required")
+)
+
+
+func Validate(req *CreateEventRequest) error {
+	if req == nul{
+		return ErrInvalidEventData
+	}
+	if req.Type == "" {
+		return ErrInvalidEventType
+	}
+	if req.Data == nil{
+		req.Data = make(map[string]interface{})
+	}
+	return nil
+}
