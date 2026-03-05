a43a4f8e733bf18453d6eb49fb127b3d455aaa3e
Ajao Yussuf <ajaoyussufayo@gmail.com>
2026-03-05 12:28:43 +0100

    middleware updated

--- internal/api/middleware.go
+++ internal/api/middleware.go
@@ -1,17 +1,36 @@
 package api
 
 import (
+	"fmt"
 	"net/http"
 	"time"
+
+	"github.com/carissaayo/go-event-distributed/internal/logger"
+	"github.com/carissaayo/go-event-distributed/internal/metrics"
+	"go.uber.org/zap"
 )
 
 func RequestLogger(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		start := time.Now()
 		ww := &responseWriter{ResponseWriter: w, status: 200}
+
 		next.ServeHTTP(ww, r)
-		_ = start
-		_ = ww.status
+
+		duration := time.Since(start)
+
+		logger.Log.Info("http request",
+			zap.String("method", r.Method),
+			zap.String("path", r.URL.Path),
+			zap.Int("status", ww.status),
+			zap.Duration("duration", duration),
+		)
+
+		metrics.HTTPRequestDuration.WithLabelValues(
+			r.Method,
+			r.URL.Path,
+			fmt.Sprintf("%d", ww.status),
+		).Observe(duration.Seconds())
 	})
 }
 
